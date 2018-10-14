9103f45ce41c5206bb26ec9faefabb884a592440
Amaury <Amaury@Amaury-PC>
2018-10-14 00:47:43 -0300

    Exemplos do diagrama de atividades adicionado

---
+++ ref/src/test/java/com/ref/ADParserTest.java
@@ -0,0 +1,78 @@
+package com.ref;
+
+public class ADParserTest {
+
+	/*
+	 * Criar teste para cada parte do diagrama de atividades
+	 * */
+	
+	/*
+	 * Teste de Tradução dos tipos de elementos
+	 * */
+	public void TestDefineTypesElements() {
+		/*
+		 * Initial node
+		 * Action node
+		 * Decision node
+		 * Merge node
+		 * Call behavior
+		 * Join node
+		 * Fork node
+		 * Final node
+		 * 
+		 * */
+	}
+	
+	/*
+	 * Teste de Tradução do processo Node_"getname"
+	 * */
+	public void TestDefineProcessNode() {
+		
+	}
+	
+	
+	/*
+	 * Teste de Tradução do processo de memória
+	 * */
+	public void TestDefineProcessMemory() {
+		
+	}
+	
+	
+	/*
+	 * Teste de Tradução do processo TokenManager
+	 * */
+	public void TestDefineProcessTokenManager() {
+		
+	}
+	
+	/*
+	 * Teste de Tradução do processo Lock
+	 * */
+	public void TestDefineProcessLock() {
+		
+	}
+
+	
+	/*
+	 * Teste de Tradução do processo principal
+	 * */
+	public void TestDefineProcessMain() {
+		/*
+		 * ad process
+		 * internal_ad process
+		 * StartActivity_ad process
+		 * EndActivity_ad process
+		 * END_DIAGRAM_ad process
+		 * MAIN process
+		 * LOOP process
+		 * */
+	}
+	
+	/*
+	 * Teste de Tradução dos canais e dados
+	 * */
+	public void TestDefineChannels() {
+		
+	}
+}
